471afb63bf1621b4c3f020f7079826dd501d59d3
HyperLabs-1 <propraveen9629@gmail.com>
2026-06-05 13:50:07 +0530

    Restore original MainActivity

--- app/src/main/kotlin/com/music/vivi/MainActivity.kt
+++ app/src/main/kotlin/com/music/vivi/MainActivity.kt
@@ -190,6 +190,7 @@ import com.hyperlabs.melo.ui.menu.YouTubeSongMenu
 import com.hyperlabs.melo.ui.player.BottomSheetPlayer
 import com.hyperlabs.melo.ui.screens.Screens
 import com.hyperlabs.melo.ui.screens.SettingDialoge
+import com.hyperlabs.melo.ui.screens.WelcomeDialog
 import com.hyperlabs.melo.ui.screens.navigationBuilder
 import com.hyperlabs.melo.ui.screens.settings.DarkMode
 import com.hyperlabs.melo.ui.screens.settings.NavigationTab
@@ -749,8 +750,12 @@ class MainActivity : ComponentActivity() {
                 val snackbarHostState = remember { SnackbarHostState() }
                 var showSettingDialoge by remember { mutableStateOf(false) }
 
+                val (lastOpenedVersionCode, setLastOpenedVersionCode) = rememberPreference(com.hyperlabs.melo.constants.LastOpenedVersionCodeKey, -1)
+                var showWelcomeDialog by remember { mutableStateOf(false) }
+
                 LaunchedEffect(lastOpenedVersionCode) {
                     if (lastOpenedVersionCode < BuildConfig.VERSION_CODE) {
+                        showWelcomeDialog = true
                     }
                 }
 
@@ -1192,6 +1197,13 @@ class MainActivity : ComponentActivity() {
                             homeViewModel = homeViewModel
                         )
                     }
+
+                    if (showWelcomeDialog) {
+                        WelcomeDialog(
+                            onDismissRequest = {
+                                showWelcomeDialog = false
+                                setLastOpenedVersionCode(BuildConfig.VERSION_CODE)
+                            }
                         )
                     }
                 }
